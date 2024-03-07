d541b6bfe59d7612002a3828317a4ca0fa1fb891
Vinícius Santos <vinisantos2008vs@gmail.com>
2024-03-07 11:09:53 -0300

    nova tarefa: alterando texto da label com clique

---
+++ atividades/tarefa-07-03/tarefa-07-03/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace tarefa_07_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Narrow", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.BlueViolet;
+            label1.Location = new Point(317, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(133, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Hello World!";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(334, 68);
+            button1.Name = "button1";
+            button1.Size = new Size(99, 38);
+            button1.TabIndex = 1;
+            button1.Text = "Trocar texto";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(336, 41);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(97, 23);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.enzo;
+            ClientSize = new Size(921, 504);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private TextBox textBox1;
+    }
+}
